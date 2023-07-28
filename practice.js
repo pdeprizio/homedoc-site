01675fd305b7de3a29d0da517b026cb6a1ccc673
pdeprizio <paigedeprizio@gmail.com>
2023-07-28 19:38:09 -0400

    Fixed menu responsiveness

--- practice.js
+++ practice.js
@@ -1,1 +1,6 @@
-console.log(hello);
+const hamburger = document.getElementById('hamburger');
+const menu = document.getElementById('menu');
+
+hamburger.addEventListener('click', () => {
+  menu.classList.toggle('show');
+});
