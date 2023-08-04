152bcac0496989b7d5977a7ff97408a58166bcae
pdeprizio <paigedeprizio@gmail.com>
2023-08-04 17:02:26 -0400

    workin on addinbefore/afters. saving this 4 slides

--- practice.js
+++ practice.js
@@ -1,6 +1,115 @@
+import "https://flackr.github.io/scroll-timeline/dist/scroll-timeline.js"  
+
+const scrollTracker = document.querySelector(".scroll-tracker");
+
+const scrollTrackingTimeline = new ScrollTimeline({
+    source: document.scrollingElement,
+    orientation: "block",
+    scrollOffsets: [CSS.percent(0), CSS.percent(100)],
+})
+ 
+scrollTracker.animate(
+    {
+      transform: ['scaleX(0)', 'scale(1)']  
+    },
+    {
+        duration: 1,
+        timeline: scrollTrackingTimeline,
+    }
+)
+ 
+  
+function slide(){
+  let slideValue = document.getElementById("slider").value;
+  document.getElementById("my-img").style.clipPath = "polygon(0 0," + slideValue + "% 0," + slideValue + "% 100%, 0 100%)";
+  console.log("polygon(0 0," + slideValue + "% 0," + slideValue + "% 100%, 0 100%)");
+}
+
+function slide2(){
+  let slideValue = document.getElementById("slider2").value;
+  document.getElementById("my-img2").style.clipPath = "polygon(0 0," + slideValue + "% 0," + slideValue + "% 100%, 0 100%)";
+  console.log("polygon(0 0," + slideValue + "% 0," + slideValue + "% 100%, 0 100%)");
+}
+
+function slide3(){
+  let slideValue = document.getElementById("slider3").value;
+  document.getElementById("my-img3").style.clipPath = "polygon(0 0," + slideValue + "% 0," + slideValue + "% 100%, 0 100%)";
+  console.log("polygon(0 0," + slideValue + "% 0," + slideValue + "% 100%, 0 100%)");
+}
+
+
+
 const hamburger = document.getElementById('hamburger');
 const menu = document.getElementById('menu');
 
 hamburger.addEventListener('click', () => {
   menu.classList.toggle('show');
 });
+
+
+
+
+
+
+
+
+
+
+
+ 
+var responsiveSlider = function() {
+
+    var slider = document.getElementById("slider");
+    var sliderWidth = slider.offsetWidth;
+    var slideList = document.getElementById("slideWrap");
+    var count = 1;
+    var items = slideList.querySelectorAll("li").length;
+    var prev = document.getElementById("prev");
+    var next = document.getElementById("next");
+    
+    window.addEventListener('resize', function() {
+      sliderWidth = slider.offsetWidth;
+    });
+    
+    var prevSlide = function() {
+      if(count > 1) {
+        count = count - 2;
+        slideList.style.left = "-" + count * sliderWidth + "px";
+        count++;
+      }
+      else if(count = 1) {
+        count = items - 1;
+        slideList.style.left = "-" + count * sliderWidth + "px";
+        count++;
+      }
+    };
+    
+    var nextSlide = function() {
+      if(count < items) {
+        slideList.style.left = "-" + count * sliderWidth + "px";
+        count++;
+      }
+      else if(count = items) {
+        slideList.style.left = "0px";
+        count = 1;
+      }
+    };
+    
+    next.addEventListener("click", function() {
+      nextSlide();
+    });
+    
+    prev.addEventListener("click", function() {
+      prevSlide();
+    });
+    
+    setInterval(function() {
+      nextSlide()
+    }, 5000);
+    
+    };
+    
+    window.onload = function() {
+    responsiveSlider();  
+    }
+    
