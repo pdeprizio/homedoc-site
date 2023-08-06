5c8fc942d438fda429fa434ca2cfc8950f9a73b9
pdeprizio <paigedeprizio@gmail.com>
2023-08-06 10:46:08 -0400

    website working, adding before/fters

--- practice.js
+++ practice.js
@@ -19,23 +19,6 @@ scrollTracker.animate(
 )
  
   
-function slide(){
-  let slideValue = document.getElementById("slider").value;
-  document.getElementById("my-img").style.clipPath = "polygon(0 0," + slideValue + "% 0," + slideValue + "% 100%, 0 100%)";
-  console.log("polygon(0 0," + slideValue + "% 0," + slideValue + "% 100%, 0 100%)");
-}
-
-function slide2(){
-  let slideValue = document.getElementById("slider2").value;
-  document.getElementById("my-img2").style.clipPath = "polygon(0 0," + slideValue + "% 0," + slideValue + "% 100%, 0 100%)";
-  console.log("polygon(0 0," + slideValue + "% 0," + slideValue + "% 100%, 0 100%)");
-}
-
-function slide3(){
-  let slideValue = document.getElementById("slider3").value;
-  document.getElementById("my-img3").style.clipPath = "polygon(0 0," + slideValue + "% 0," + slideValue + "% 100%, 0 100%)";
-  console.log("polygon(0 0," + slideValue + "% 0," + slideValue + "% 100%, 0 100%)");
-}
 
 
 
@@ -47,69 +30,3 @@ hamburger.addEventListener('click', () => {
 });
 
 
-
-
-
-
-
-
-
-
-
- 
-var responsiveSlider = function() {
-
-    var slider = document.getElementById("slider");
-    var sliderWidth = slider.offsetWidth;
-    var slideList = document.getElementById("slideWrap");
-    var count = 1;
-    var items = slideList.querySelectorAll("li").length;
-    var prev = document.getElementById("prev");
-    var next = document.getElementById("next");
-    
-    window.addEventListener('resize', function() {
-      sliderWidth = slider.offsetWidth;
-    });
-    
-    var prevSlide = function() {
-      if(count > 1) {
-        count = count - 2;
-        slideList.style.left = "-" + count * sliderWidth + "px";
-        count++;
-      }
-      else if(count = 1) {
-        count = items - 1;
-        slideList.style.left = "-" + count * sliderWidth + "px";
-        count++;
-      }
-    };
-    
-    var nextSlide = function() {
-      if(count < items) {
-        slideList.style.left = "-" + count * sliderWidth + "px";
-        count++;
-      }
-      else if(count = items) {
-        slideList.style.left = "0px";
-        count = 1;
-      }
-    };
-    
-    next.addEventListener("click", function() {
-      nextSlide();
-    });
-    
-    prev.addEventListener("click", function() {
-      prevSlide();
-    });
-    
-    setInterval(function() {
-      nextSlide()
-    }, 5000);
-    
-    };
-    
-    window.onload = function() {
-    responsiveSlider();  
-    }
-    
